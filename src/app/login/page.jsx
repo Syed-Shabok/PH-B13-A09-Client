4026bb0a164b81446e91d40559136f8c4ea9bee9
Syed-Shabok <syed.shabok@gmail.com>
2026-05-22 22:13:43 +0600

    Wrapped Login & Register Client in Suspense.

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,26 @@
+import { Suspense } from "react";
 import LoginClient from "./LoginClient";
+import { BarLoader } from "react-spinners";
 
 export const metadata = {
   title: "Login | IdeaVault",
 };
 
 const LoginPage = () => {
-  return <LoginClient />;
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="flex flex-col gap-3 items-center justify-center font-mono text-xs uppercase tracking-widest text-[var(--card-text-muted)] animate-pulse">
+            <BarLoader color="#249E94" />
+            // Loading...
+          </div>
+        </div>
+      }
+    >
+      <LoginClient />
+    </Suspense>
+  );
 };
 
 export default LoginPage;
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,11 +1,26 @@
+import { Suspense } from "react";
 import RegisterClient from "./RegisterClient";
+import { BarLoader } from "react-spinners";
 
 export const metadata = {
   title: "Register | IdeaVault",
 };
 
 const RegisterPage = () => {
-  return <RegisterClient />;
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="flex flex-col gap-3 items-center justify-center font-mono text-xs uppercase tracking-widest text-[var(--card-text-muted)] animate-pulse">
+            <BarLoader color="#249E94" />
+            // Loading...
+          </div>
+        </div>
+      }
+    >
+      <RegisterClient />
+    </Suspense>
+  );
 };
 
 export default RegisterPage;
